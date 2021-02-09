b0b9725e0f6ff307858b5588191991b3daaf107d
leomark92 <markobogush@gmail.com>
2021-02-09 22:11:57 +0200

    next_part_of_work1

---
+++ docs/js/script.js
@@ -0,0 +1,14 @@
+const   menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close'),
+        deli = document.querySelector('.deli');
+
+deli.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
